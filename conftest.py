e77719ca5e325f31b76670aedb05df728e791cca
RomanKasko <46456972+RomanKasko@users.noreply.github.com>
2020-09-24 19:07:58 +0300

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,39 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name',\
+                     action = 'store',\
+                     default = "chrome",\
+                     help = "Choose browser: chrome of firefox")
+    parser.addoption('--language',\
+                     action = 'store',\
+                     default = 'en',\
+                     help="Choose language: ru,en, ... (etc.)")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(options=options)
+
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages",user_language)
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        print("Browser should be chrome or firefox")
+    yield browser
+    ###################
+    time.sleep(10)
+    ###################
+    print("\nquit browser..")
+    browser.quit()
